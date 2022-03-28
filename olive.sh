36527ce36ce425122cefcf27c66bc40afc91481e
Jprimero15 <jprimero155@gmail.com>
2022-03-28 21:25:51 +0800

    olive is ba

--- olive.sh
+++ olive.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+ #!/bin/bash
 
 # Clone device tree
-git clone https://github.com/SHRP-Devices/android_device_xiaomi_olive device/xiaomi/olive
+git clone https://github.com/Jprimero15/device_xiaomi_olive-shrp device/xiaomi/olive
 
-# Setup the build
-export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
-lunch omni_olive-eng
+lunch twrp_olive-eng
 
 # Build SHRP!
 mka recoveryimage
